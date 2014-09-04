772b5147692f446334a74bd5424f253b6bef6c20
lvsavova <lvsavova93@gmail.com>
2014-09-04 22:48:10 +0300

    Update: Added docstings to classes and methods

--- gui.py
+++ gui.py
@@ -13,19 +13,38 @@ selected_type = ""
 
 
 def callback_dir(entry):
+    """
+    Callback method for browse buttons that open
+    a directory chooser dialog. After directory is chosen
+    writes its full path to the respected text entry.
+    """
     entry.configure(state='normal')
+    entry.delete(0,END)
     directory_name = askdirectory()
     entry.insert(0, directory_name)
     entry.configure(state='disabled')
 
 
 def callback_rb(type, content_entry_state):
+    """
+    Callback method for the radio buttons that hold
+    information on type of reorganization. Writes
+    the type in a global variable selected_type.
+    """
     content_entry.configure(state=content_entry_state)
     global selected_type
     selected_type = type
 
 
 def organize():
+    """
+    Callback method for the Organize button. Edits the
+    config.ini file according to the values from the gui elements.
+    Raises exceptions upon empty entries or unselected reorganization
+    type. At last calls the main method in organizefiles.py, notifies
+    the user if something went wrong and when the job has successfully
+    finished
+    """
     if selected_type == "":
         showwarning('Ok', 'Please choose organization type!')
         raise Exception('Organization type not set')
@@ -33,8 +52,8 @@ def organize():
         showwarning('Ok', 'Please choose target and source folders!')
         raise Exception('Target or Source directory not set')
     if selected_type == "content" and content_entry.get() == "":
-        showwarning('Ok', 'Please choose strings by which docx files are organized!')
-        raise Exception('No strings by which to perform content re-organization ')
+        showwarning('Ok', 'Choose strings by which docx files are organized!')
+        raise Exception('No strings by which to perform content organization ')
 
     config = configparser.ConfigParser()
     config.read(r'config.ini')
--- organizer.py
+++ organizer.py
@@ -2,7 +2,6 @@
 import os.path
 import sys
 import re
-import mimetypes
 from collections import defaultdict
 from documenthandler import DocumentHandler
 
@@ -17,14 +16,28 @@ FILE_TYPES = {'Presentations': ['pptx', 'ppt'],
 
 
 class Organizer:
-
+    """
+    Implements the logic by which files are organized.
+    Has an attribute - list of file names, which are
+    indeed the file names of the directory we would like
+    to have organized (source directory)
+    """
     file_list = []
 
     def __init__(self, directory):
+        """
+        Initializes the source directory associated
+        with the Organizer object.
+        """
         self.directory = directory
         self.file_list = os.listdir(directory)
 
     def organize_by_extension(self):
+        """
+        Iterates through all the files in the source directory and
+        returns a dictionary with key the file extension and value -
+        list of file names that have this extension
+        """
         files_by_extensions = defaultdict(list)
         for file in self.file_list:
                 extension = os.path.splitext(file)[1].split('.')[-1]
@@ -34,6 +47,12 @@ class Organizer:
         return files_by_extensions
 
     def organize_by_type(self):
+        """
+        Iterates through all the files in the source directory and
+        returns a dictionary with key the file type and value -
+        list of file names that are of this type.
+        Types of files are defined in FILES_TYPE dict.
+        """
         files_by_type = defaultdict(list)
         files_by_extensions = self.organize_by_extension()
         for extension in files_by_extensions:
@@ -43,12 +62,18 @@ class Organizer:
         return files_by_type
 
     def organize_by_content(self, match_strings):
+        """
+        Iterates through all the files in the source directory and
+        returns a dictionary with key the string we want to
+        match and values- the docx files containing the string
+        """
         files_by_content = defaultdict(list)
         files_by_extensions = self.organize_by_extension()
         docx_files = files_by_extensions['docx']
         doc_handler = DocumentHandler()
         for file in docx_files:
             for string in match_strings:
-                if doc_handler.search_content(self.directory + "\\" + file, string) is True:
+                if doc_handler.search_content(self.directory +
+                                              "\\" + file, string) is True:
                     files_by_content[string].append(file)
         return files_by_content
